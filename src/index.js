b5e91954ad1f8443928331b25b9a087dfb3a2ce0
plungingChode <szigeti.peter23@gmail.com>
2020-11-09 00:19:41 +0100

    Fix layout issues
    Unify HTML and CSS naming scheme (lowercase-dashed)
    
    Compress sample script
    Add autorun after edit (optional)
    Add autosave after edit
    Persist user preferences (last document, autorun, quiet mode)
    
    Add download handling to PHP (not yet used)

--- src/index.js
+++ src/index.js
@@ -1,116 +1,142 @@
 const CodeMirror = require("../node_modules/codemirror/lib/codemirror");
 const simple = require("../node_modules/codemirror/mode/simple/simple");
+import "../node_modules/codemirror/keymap/sublime.js";
 import "../node_modules/codemirror/lib/codemirror.css";
 import "./ambiance.css";
 import "./index.css";  
 import "./xfst.php";
 import "./xfst";
 
-const sampleScript = `define Nm[
-	N -> m || _ p 
-];
+const sampleScript = `define Nm[ N -> m || _ p ];
+define pm[ p -> m || m _ ];
 
-define pm[ 
-	p -> m || m _ 
-];
-
-regex [
-	Nm .o. pm
-];
+regex [ Nm .o. pm ];
 
 apply down
-
 kaNpat
 kammat`
 
 CodeMirror.defineSimpleMode("xfst", {
     start: [
         {regex: /\bdefine|regex\b/, token: "keyword"},
-        {regex: /#[^\\.].*/, token: "comment"},
+        {regex: /(^|[^\.])#.*/, token: "comment"},
         {regex: /\.#\.|\.o\.|@|_|->|\|\|/, token: "operator"},
         {regex: /\bapply (down|up)\b/, token: "atom"}
     ]
 });
 
-const left = document.getElementById("leftSide");
-const quiet = document.getElementById("qSwitchCheck");
-const output = document.getElementById("xfstOutput");
-const editor = CodeMirror(
-    document.getElementById("xfstInput"), 
+let persist;
+
+const tgl_quiet = document.getElementById("toggle-quiet");
+const tgl_autorun = document.getElementById("toggle-autorun");
+const output = document.getElementById("script-output");
+const input = document.getElementById("script-input");
+const editor = CodeMirror(input, 
     {
         lineNumbers: true,
         lineWrapping: true,
+        keyMap: "sublime",
         theme: "ambiance",
-        mode: "xfst",
-        extraKeys: {
-            "[": autocompleteBrackets,
-        }
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        mode: "xfst"
     });
-
-function autocompleteBrackets() {
-    const start = editor.getCursor(true);
-    editor.replaceSelection("[]", start);
-
-    start.ch += 1;
-    editor.setCursor(start);
-}
 
 async function xfst() {
     const fd = new FormData();
     fd.append("input", editor.getValue());
 
-    if (quiet.checked) {
+    if (tgl_quiet.checked) 
         fd.append("quiet", 1);
-    }
 
     const resp = await fetch("xfst.php", { method: "POST", body: fd });
     const text = await resp.text();
-    
-    fetch("xfst.php", {
-        method: "POST",
-        body: fd
+
+    let lines = text.split("\n");
+    lines = lines.map(l => {
+        if (l.startsWith("***")) {
+            return `<span class="xfst-err">${l}"</span>`;
+        }
+        else if (l.startsWith("Defined")) {
+            return `<span class="xfst-def">${l}</span>`;
+        }
+        else if (l !== "bye.") {
+            return l;
+        }
     })
-    .then(d => d.text())
-    .then(txt =>  {
-        let lines = txt.split("\n");
-        lines = lines.map(l => {
-            if (l.startsWith("***")) {
-                return `<span class="xfst-err">${l}"</span>`;
-            }
-            else if (l.startsWith("Defined")) {
-                return `<span class="xfst-def">${l}</span>`;
-            }
-            else if (l !== "bye.") {
-                return l;
-            }
-        })
-        output.innerHTML = lines.join("\n");
-    });
+    output.innerHTML = lines.join("\n");
 }
 
 document.addEventListener("keydown", e => {
     if (e.ctrlKey && (e.key === "s" || e.key === "Enter")) {
         xfst();
+        saveLocal();
         e.preventDefault();
         return;
     }
 })
 
-quiet.addEventListener("change", e => xfst());
-document.getElementById("run").addEventListener("click", e => xfst());
+tgl_quiet.addEventListener("change", e => { 
+    persist["quiet"] = e.target.value;
+    localStorage.setItem("quiet", e.target.value);
+    xfst();
+});
+tgl_autorun.addEventListener("change", e => {
+    persist["autorun"] = e.target.value;
+    localStorage.setItem("autorun", e.target.value);
+});
+document.getElementById("run").addEventListener("click", e => { 
+    output.innerHTML = "";
+    setTimeout(xfst, 100);
+});
 
 document.getElementById("fload").addEventListener("change", e => {
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.onload = (function(f) {
         return function(e) {
-            editor.setValue(e.target.result)
+            editor.setValue(e.target.result);
+            xfst();
         }
     })(file);
     reader.readAsText(file);
 })
 
-// TODO: add download option
+function saveLocal() {
+    localStorage.setItem("script", editor.getValue());
+    console.log("saved");
+}
+
+let autosave = null;
+let autorun = null;
+CodeMirror.on(editor, "change", () => {
+    clearTimeout(autosave);
+    autosave = setTimeout(saveLocal, 1000);
+
+    if (persist["autorun"]) {
+        clearTimeout(autorun);
+        autorun = setTimeout(xfst, 1000);
+    }
+});
+
+window.addEventListener("load", () => {
+    persist = {
+        "script": localStorage.getItem("script"),
+        "quiet": localStorage.getItem("quiet") || false,
+        "autorun": localStorage.getItem("autorun") || true
+    }
+
+    if (persist["script"] === null) 
+        persist["script"] = sampleScript;
+
+    editor.setValue(persist["script"]);
+    tgl_autorun.value = persist["autorun"];
+    tgl_quiet.value = persist["quiet"];
+    setEditorSize();
+    xfst();
+})
+
+// // TODO: add download option
 // document.getElementById("fsave").addEventListener("change", e => {
 //     var element = document.createElement('a');
 //     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("asdsad"));
@@ -124,22 +150,10 @@ document.getElementById("fload").addEventListener("change", e => {
 //     document.body.removeChild(element);
 // })
 
-// FIXME: fix left & right side sizing
 function setEditorSize() {
-    if (window.innerWidth < 1201) {
-        left.style.height = window.innerHeight / 2 - 20 + "px";
-    }
-    else {
-        left.style.height = document.body.offsetHeight - 115 + "px";
-    }
+    input.style.height = output.offsetHeight + "px";
 }
 
 window.addEventListener("resize", () => {
     setEditorSize();
-    console.log(window.innerHeight / 2);
-    console.log(window.innerWidth);
 })
-
-setEditorSize();
-editor.setValue(sampleScript);
-xfst();
--- src/xfst.php
+++ src/xfst.php
@@ -1,9 +1,32 @@
 <?php 
-    setlocale(LC_CTYPE, "hu_HU.UTF-8");    
+    if (isset($_REQUEST["download-text"])) {
+        $tempName = tempnam(sys_get_temp_dir(), 'script');
+        $file = fopen($tmpName, 'w');
 
-    $input = escapeshellarg($_POST["input"]);
-    $quiet = isset($_POST["quiet"]) ? "-q" : "";
+        fputs($_POST["download-text"]);
+        fclose($file);
 
-    $output = shell_exec("echo -e ".$input." | ./xfst ".$quiet." 2>&1");
-    echo $output;
+        header("Content-Description: File Transfer");
+        header("Content-Type: text/plain");
+        header("Content-Disposition: attachment; filename=script.xfst");
+        header("Content-Transfer-Encoding: binary");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate");
+        header("Pragma: public");
+        header("Content-Length: ".filesize($tmpName));
+
+        ob_clean();
+        flush();
+        readfile($tmpName);
+        unlink($tmpName);
+    }
+    else if (isset($_POST["input"])) {
+        setlocale(LC_CTYPE, "hu_HU.UTF-8");    
+
+        $input = escapeshellarg($_POST["input"]);
+        $quiet = isset($_POST["quiet"]) ? "-q" : "";
+    
+        $output = shell_exec("echo -e ".$input." | ./xfst ".$quiet." 2>&1");
+        echo $output;
+    }
 ?>
